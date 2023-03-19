cf127b6843e2be014164e6be7b588dce2c28987e
Ker_4 <ker_4@vk.com>
2023-03-19 20:19:14 +0300

    .HomeWork2: add base class Checkpoint for EntryPoint & ExitPoint

---
+++ HomeWork2/src/baseClasses/Checkpoint.java
@@ -0,0 +1,68 @@
+package baseClasses;
+
+import entities.Car;
+import interfaces.ICheckpoint;
+
+import java.util.ArrayList;
+
+public class Checkpoint implements ICheckpoint {
+
+    /**
+     * Номер пропускного пункта
+     */
+    private Integer number;
+
+    /**
+     * Описание пропускного пункта
+     */
+    private String description;
+
+    /**
+     * Список проехавших машин
+     */
+    private ArrayList<Car> pastCars;
+
+    @Override
+    public int getNumber() {
+        return this.number;
+    }
+
+    /**
+     * Устанавливает номер пропускного пункта с учётом проверки на отрицательное значение
+     * @param number -- номер пункта
+     */
+    protected void setNumber(Integer number) {
+        if (number >= 0)
+            this.number = number;
+        else
+            System.err.println("Номер пропускного пункта не может быть отрицательным.");
+    }
+
+    @Override
+    public String getDescription() {
+        return this.description;
+    }
+
+    @Override
+    public ArrayList<Car> getPastCars() {
+        return this.pastCars;
+    }
+
+    /**
+     * Пустой конструктор класса {@link Checkpoint}
+     */
+    public Checkpoint() {
+    }
+
+    /**
+     * Конструктор класса {@link Checkpoint}
+     * @param number -- номер пункта
+     * @param description -- описание пункта
+     * @param pastCars -- список проехавших автомобилей
+     */
+    protected Checkpoint(Integer number, String description, ArrayList<Car> pastCars) {
+        setNumber(number);
+        this.description = description;
+        this.pastCars = pastCars;
+    }
+}
