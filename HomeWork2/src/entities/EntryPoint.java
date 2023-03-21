b1aa15662a18588186e5d1d7271d6520bfe9f377
Ker_4 <ker_4@vk.com>
2023-03-21 23:26:41 +0300

    .HomeWork2: get constructors back in EntryPoint

--- HomeWork2/src/entities/EntryPoint.java
+++ HomeWork2/src/entities/EntryPoint.java
@@ -2,7 +2,6 @@ package entities;
 
 import baseClasses.Checkpoint;
 
-import java.util.Arrays;
 import java.util.Date;
 
 /**
@@ -10,7 +9,38 @@ import java.util.Date;
  */
 public class EntryPoint extends Checkpoint {
 
+    /**
+     * Конструктор класса {@link EntryPoint}
+     *
+     * @param parking     -- парковка, на которой находится пропускной пункт
+     * @param number      -- номер пункта
+     * @param description -- описание
+     * @param pastCars    -- список проехавших машин
+     */
+    public EntryPoint(Parking parking, Integer number, String description, Car[] pastCars) {
+        super(parking, number, description, pastCars);
+    }
 
+    /**
+     * Конструктор класса {@link EntryPoint}
+     *
+     * @param parking     -- парковка, на которой находится пропускной пункт
+     * @param number      -- номер пункта
+     * @param description -- описание
+     */
+    public EntryPoint(Parking parking, Integer number, String description) {
+        super(parking, number, description, null);
+    }
+
+    /**
+     * Конструктор класса {@link EntryPoint}
+     *
+     * @param parking -- парковка, на которой находится пропускной пункт
+     * @param number  -- номер пункта
+     */
+    public EntryPoint(Parking parking, Integer number) {
+        super(parking, number, null, null);
+    }
 
 
     /**
@@ -30,10 +60,6 @@ public class EntryPoint extends Checkpoint {
      * @param car машина, которую необходимо добавить
      */
     protected void passThisCar(Car car) {
-        if (Arrays.stream(pastCars).anyMatch(x -> x == car)){
-            System.err.println("На парковке не может быть 2 машины с одинаковыми номерами.");
-            return;
-        }
         // нахождение индекса реального последнего объекта в массиве c избыточным кол-вом ячеек
         int lastCarIndex = 0;
         for (int i = 0; i < pastCars.length - 1; i++)
