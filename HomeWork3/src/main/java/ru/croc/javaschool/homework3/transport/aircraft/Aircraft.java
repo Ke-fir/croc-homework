1f17d42776f2c6a9ea6715dde439947a3cacf248
Ker_4 <ker_4@vk.com>
2023-04-01 19:59:24 +0300

    .HomeWork3: add basic class for air transport

---
+++ HomeWork3/src/main/java/ru/croc/javaschool/homework3/transport/aircraft/Aircraft.java
@@ -0,0 +1,49 @@
+package ru.croc.javaschool.homework3.transport.aircraft;
+
+import ru.croc.javaschool.homework3.transport.Transport;
+
+/**
+ * Abstract class of air transport that extends basic transport
+ */
+public abstract class Aircraft extends Transport {
+    private int numberOfSeats;
+    private int numberOfEngines;
+
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    public void setNumberOfSeats(int numberOfSeats) {
+        if (numberOfSeats > 0)
+            this.numberOfSeats = numberOfSeats;
+        else
+            System.err.println("Invalid number of seats");
+    }
+
+    public int getNumberOfEngines() {
+        return numberOfEngines;
+    }
+
+    public void setNumberOfEngines(int numberOfEngines) {
+        if (numberOfEngines > 0)
+            this.numberOfEngines = numberOfEngines;
+        else
+            System.err.println("Invalid number of seats");
+    }
+
+    /**
+     * Aircraft constructor.
+     *
+     * @param id              unique positive integer
+     * @param rentalPrice     price in rubbles
+     * @param brand           name of brand
+     * @param model           name of model
+     * @param numberOfEngines positive number of engines
+     * @param numberOfSeats   positive number of seats
+     */
+    public Aircraft(int id, double rentalPrice, String brand, String model, int numberOfEngines, int numberOfSeats) {
+        super(id, rentalPrice, brand, model);
+        setNumberOfEngines(numberOfEngines);
+        setNumberOfSeats(numberOfSeats);
+    }
+}
