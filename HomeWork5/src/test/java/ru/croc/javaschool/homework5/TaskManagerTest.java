a780caad31e82401c5289d7c58fbd9b47c6454ec
Ker_4 <ker_4@vk.com>
2023-04-11 18:57:24 +0300

    .HomeWork5: add TaskManager test

---
+++ HomeWork5/src/test/java/ru/croc/javaschool/homework5/TaskManagerTest.java
@@ -0,0 +1,14 @@
+package ru.croc.javaschool.homework5;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test class for task manager.
+ */
+public class TaskManagerTest {
+
+    @Test
+    public void addTaskTest(){
+
+    }
+}
