e801ed461f7554dbf358630083f02afe8487c3d1
Ker_4 <ker_4@vk.com>
2023-04-13 10:54:59 +0300

    .HomeWork5: delete broken (for idea) files

--- HomeWork5/src/main/java/ru/croc/javaschool/homework5/ProgrammPresentation.java
+++
@@ -1,8 +0,0 @@
-package ru.croc.javaschool.homework5;
-
-public class ProgrammPresentation {
-    public static void main(String[] args) {
-        ConsoleApi api = new ConsoleApi();
-        api.run();
-    }
-}
