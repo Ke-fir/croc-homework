8e52904a2eda4ba940119a9c673e58ecaa7e3025
Ker_4 <ker_4@vk.com>
2023-04-13 10:55:23 +0300

    .HomeWork5: update ProgramPresentation

--- HomeWork5/src/main/java/ru/croc/javaschool/homework5/ProgramPresentation.java
+++ HomeWork5/src/main/java/ru/croc/javaschool/homework5/ProgramPresentation.java
@@ -1,5 +1,7 @@
 package ru.croc.javaschool.homework5;
 
+import ru.croc.javaschool.homework5.console.ConsoleApi;
+
 public class ProgramPresentation {
     public static void main(String[] args) {
         ConsoleApi api = new ConsoleApi();
