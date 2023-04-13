e6c1cd37b2c219abd57542382be4b6d40364448c
Ker_4 <ker_4@vk.com>
2023-04-13 10:07:55 +0300

    .HomeWork5: add class for program presentation

---
+++ HomeWork5/src/main/java/ru/croc/javaschool/homework5/ProgrammPresentation.java
@@ -0,0 +1,8 @@
+package ru.croc.javaschool.homework5;
+
+public class ProgrammPresentation {
+    public static void main(String[] args) {
+        ConsoleApi api = new ConsoleApi();
+        api.run();
+    }
+}
