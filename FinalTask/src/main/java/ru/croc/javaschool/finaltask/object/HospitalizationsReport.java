9c26e6d8608577f952f6ccf99986fa5c62e97786
Ker_4 <ker_4@vk.com>
2023-05-14 18:06:48 +0300

    .FinalTask: add objects

---
+++ FinalTask/src/main/java/ru/croc/javaschool/finaltask/object/HospitalizationsReport.java
@@ -0,0 +1,67 @@
+package ru.croc.javaschool.finaltask.object;
+
+import java.time.LocalDate;
+
+/**
+ * The class responsible for daily statistics about count of hospitalized and discharged people.
+ * Contains properties date, hospitalizationsCount, dischargedCount.
+ */
+public class HospitalizationsReport {
+    /**
+     * The date of report.
+     */
+    private LocalDate date;
+
+    /**
+     * The number of hospitalizations on that date.
+     */
+    private int hospitalizationsCount;
+
+    /**
+     * The number of discharged patients on that date.
+     */
+    private int dischargedCount;
+
+    /**
+     * Creates a HospitalizationsReport instance.
+     *
+     * @param date                  The date of report
+     * @param hospitalizationsCount The number of hospitalizations on that date
+     * @param dischargedCount       The number of discharged patients on that date
+     */
+    public HospitalizationsReport(LocalDate date, int hospitalizationsCount, int dischargedCount) {
+        this.date = date;
+        this.hospitalizationsCount = hospitalizationsCount;
+        this.dischargedCount = dischargedCount;
+    }
+
+    /**
+     * Default constructor.
+     */
+    public HospitalizationsReport() {
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public int getHospitalizationsCount() {
+        return hospitalizationsCount;
+    }
+
+    public void setHospitalizationsCount(int hospitalizationsCount) {
+        this.hospitalizationsCount = hospitalizationsCount;
+    }
+
+    public int getDischargedCount() {
+        return dischargedCount;
+    }
+
+    public void setDischargedCount(int dischargedCount) {
+        this.dischargedCount = dischargedCount;
+    }
+}
---
+++ FinalTask/src/main/java/ru/croc/javaschool/finaltask/object/InfectionsReport.java
@@ -0,0 +1,58 @@
+package ru.croc.javaschool.finaltask.object;
+
+import java.time.LocalDate;
+
+/**
+ * The class responsible for daily statistics about count of infected and recovered people.
+ * Contains properties date, infectionCasesCount, recoveryCasesCount.
+ */
+public class InfectionsReport {
+
+    private LocalDate date;
+    private int infectionCasesCount;
+    private int recoveryCasesCount;
+
+    /**
+     * Creates a InfectionsReport instance.
+     *
+     * @param date                The date of the report
+     * @param infectionCasesCount The number of new infection cases
+     * @param recoveryCasesCount  The number of recoveries
+     */
+    public InfectionsReport(LocalDate date, int infectionCasesCount, int recoveryCasesCount) {
+        this.date = date;
+        this.infectionCasesCount = infectionCasesCount;
+        this.recoveryCasesCount = recoveryCasesCount;
+    }
+
+    /**
+     * Default constructor.
+     */
+    /* Is used for serialization */
+    public InfectionsReport() {
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public int getInfectionCasesCount() {
+        return infectionCasesCount;
+    }
+
+    public void setInfectionCasesCount(int infectionCasesCount) {
+        this.infectionCasesCount = infectionCasesCount;
+    }
+
+    public int getRecoveryCasesCount() {
+        return recoveryCasesCount;
+    }
+
+    public void setRecoveryCasesCount(int recoveryCasesCount) {
+        this.recoveryCasesCount = recoveryCasesCount;
+    }
+}
